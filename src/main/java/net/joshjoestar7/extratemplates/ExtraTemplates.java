3e987e0937449a40750b8350f91331f9f0359f43
JoshJoestar7 <dextreygaymer@gmail.com>
2025-10-30 21:13:48 -0400

    Test

---
+++ src/main/java/net/joshjoestar7/extratemplates/ExtraTemplates.java
@@ -0,0 +1,16 @@
+package net.joshjoestar7.extratemplates;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExtraTemplates implements ModInitializer {
+	public static final String MOD_ID = "extra-templates";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/joshjoestar7/extratemplates/ExtraTemplatesClient.java
@@ -0,0 +1,10 @@
+package net.joshjoestar7.extratemplates;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraTemplatesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
